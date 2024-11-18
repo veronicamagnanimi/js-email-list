10953fe93de037d9aa0d1a78bc4953c0272b2f8f
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-18 16:00:26 +0100

    feat: bonus complete

--- js/script.js
+++ js/script.js
@@ -8,25 +8,41 @@ const eList = document.getElementById("list");
 
 let totalEmail = [];
 
-
-for (let i = 0; i < 10; i++) {
-
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-        console.log(resp.data);
-    
-    eList.innerHTML += `<div class="list-group-item list-group-item-action text-center fw-bold">${resp.data.response}</div>`;
-
-
 //evento sul bottone
 
 btn.addEventListener("click", function() {
     eList.innerHTML = "";
 
+    for (let i = 0; i < 10; i++) {
+
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+            console.log(resp.data); // restituisce l'intera risposta del server
+        
+        eList.innerHTML += `<div class="list-group-item list-group-item-action text-center fw-bold">${resp.data.response}</div>`;
+    
+    
+    
+    
+        })
+    }
+    
+
 }
 )
 
-    })
-}
+
+// for (let i = 0; i < 10; i++) {
+
+//     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+//         console.log(resp.data); // restituisce l'intera risposta del server
+    
+//     eList.innerHTML += `<div class="list-group-item list-group-item-action text-center fw-bold">${resp.data.response}</div>`;
+
+
+
+
+//     })
+// }
 
 
 
