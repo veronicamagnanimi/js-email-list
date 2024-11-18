2fdb590e36f10a8583a8431946bacd5616ed3360
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-18 15:49:48 +0100

    exercise complete + bonus wip

--- js/script.js
+++ js/script.js
@@ -8,12 +8,27 @@ const eList = document.getElementById("list");
 
 let totalEmail = [];
 
+
 for (let i = 0; i < 10; i++) {
 
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-        console.log(resp);
+        console.log(resp.data);
+    
+    eList.innerHTML += `<div class="list-group-item list-group-item-action text-center fw-bold">${resp.data.response}</div>`;
+
+
+//evento sul bottone
+
+btn.addEventListener("click", function() {
+    eList.innerHTML = "";
+
+}
+)
+
     })
 }
+
+
 
 
 
